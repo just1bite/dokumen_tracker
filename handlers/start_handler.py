b8b60f7b83f8263b6adf401c063e0fc4b8fc6250
fahri <fuahari@gmail.com>
2025-05-30 15:04:45 +0700

    add loggin pada start_handler

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -1,8 +1,11 @@
 from aiogram import types, Dispatcher
 from helpers.sambutan import kirim_sambutan
+import logging
 
 async def start_command_handler(message: types.Message):
+    logging.info("📩 /start command received")
     await kirim_sambutan(message)
 
 def register_handlers(dp: Dispatcher):
+    logging.info("✅ start_handler registered")
     dp.register_message_handler(start_command_handler, commands=["start"])
