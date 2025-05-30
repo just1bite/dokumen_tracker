f5afaee0aa614430a67b9f2f6a04a043c47cdea9
fahri <fuahari@gmail.com>
2025-05-30 13:13:21 +0700

    fix __initi__handler

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -3,9 +3,6 @@ from .list_handler import register_handlers as register_list_handlers
 from .start_handler import register_handlers as register_start_handlers
 
 def register_handlers(dp):
-    register_update_handlers(dp)
-
-def register_handlers(dp):
     register_list_handlers(dp)
     register_update_handlers(dp)
     register_start_handlers(dp)
